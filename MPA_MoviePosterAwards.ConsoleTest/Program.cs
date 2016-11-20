1dca07d63ae320d860240ba9f494925b2472fff0
金鑫 <jinxin19950105@outlook.com>
2016-11-20 23:27:53 +0800

    新增电影模块-添加电影

---
+++ MPA_MoviePosterAwards.ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using MPA_MoviePosterAwards.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPA_MoviePosterAwards.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieManager.GetMovie("1307847");
+        }
+    }
+}
